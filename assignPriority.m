07e7d31acea1b25231c9f546a62110914439f075
choobenghao <jackbenghao@gmail.com>
2026-06-16 13:10:56 +0800

    second update

--- assignPriority.m
+++ assignPriority.m
@@ -7,17 +7,13 @@ for i=1:N
     r = rand();
 
     if r <= 0.2
-
         priority(i)=1;
 
     elseif r <= 0.5
-
         priority(i)=2;
 
     else
-
         priority(i)=3;
-
     end
 
 end
--- compareScenario.m
+++ compareScenario.m
@@ -1,27 +1,23 @@
 function compareScenario(a,b)
 
-fprintf('\n');
 fprintf('Average Waiting Time\n');
-fprintf('Scenario1 : %.2f\n',a.avgWaiting);
-fprintf('Scenario2 : %.2f\n',b.avgWaiting);
+fprintf('3 Doctors : %.2f\n',a.avgWaiting);
+fprintf('4 Doctors : %.2f\n',b.avgWaiting);
 
 fprintf('\n');
+
 fprintf('Average Queue Length\n');
-fprintf('Scenario1 : %.2f\n',a.avgQueue);
-fprintf('Scenario2 : %.2f\n',b.avgQueue);
+fprintf('3 Doctors : %.2f\n',a.avgQueue);
+fprintf('4 Doctors : %.2f\n',b.avgQueue);
 
 fprintf('\n');
+
 fprintf('Doctor Utilization\n');
-fprintf('Scenario1 : %.2f%%\n',...
-        a.utilization*100);
-fprintf('Scenario2 : %.2f%%\n',...
-        b.utilization*100);
+fprintf('3 Doctors : %.2f%%\n',a.utilization*100);
+fprintf('4 Doctors : %.2f%%\n',b.utilization*100);
 
 fprintf('\n');
-fprintf('Patients Served\n');
-fprintf('Scenario1 : %d\n',...
-        a.totalServed);
-fprintf('Scenario2 : %d\n',...
-        b.totalServed);
 
-end
+fprintf('Patients Served\n');
+fprintf('3 Doctors : %d\n',a.totalServed);
+fprintf('4 Doctors : %d\n',b.totalServed);
